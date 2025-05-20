09ce0f08726dc65a665eabe6666904c0c9e95684
ENZO FELIX CARRILHO <25132408@CFP127ALUNO.LOCAL>
2025-05-20 11:26:54 -0300

    Acrescentando lógica de cálculo de subRedes

--- src/br/com/sp/senai/ipinfo/model/Ip.java
+++ src/br/com/sp/senai/ipinfo/model/Ip.java
@@ -1,5 +1,9 @@
 package br.com.sp.senai.ipinfo.model;
 
+import java.util.HashMap;
+
+import com.sun.javafx.collections.MappingChange.Map;
+
 public class Ip {
 	
 	private String endereco;
@@ -43,7 +47,6 @@ public class Ip {
 	
 	private void definirClasse(){
 		
-		
 		String inicioEndereco = endereco.substring(0, 3);
 		int inicioEnderecoint = Integer.parseInt(inicioEndereco);
 		
@@ -69,29 +72,28 @@ public class Ip {
 		
 	}
 	
+	StringBuilder novaMascaraBinario = null;
 	
 	private void definirMascara() {
 	
-		// separando o cidr do endereço ip
+		// separando o cidr do endere�o ip
 		String[] separador = endereco.split("/");
 		
 		String stringcidr = separador[1];
 		int intcidr = Integer.parseInt(stringcidr);
 		
 		// Criando um StringBuilder com 32 zeros
-		StringBuilder novaMascaraBinario = new StringBuilder("00000000000000000000000000000000");
+		novaMascaraBinario = new StringBuilder("00000000000000000000000000000000");
 
 		// Preenchendo os bits com '1' de acordo com o valor do CIDR			
-		// dentro da sintaxe (inicializando a variável i, codição da estrutura, e incremento), o loop continuará enquanto a condição for verdadeira
 		for (int i = 0; i < intcidr; i++) {
 			novaMascaraBinario.setCharAt(i, '1');
 		}
 		
-		String[] octetosMascaraBinario = new String[4];
-		String primeiroOcteto = octetosMascaraBinario[0] = novaMascaraBinario.substring(0, 8);
-		String segundoOcteto = octetosMascaraBinario[1] = novaMascaraBinario.substring(8, 16);
-		String terceiroOcteto = octetosMascaraBinario[2] = novaMascaraBinario.substring(16, 24);
-		String quartoOcteto = octetosMascaraBinario[3] = novaMascaraBinario.substring(24, 32);
+		String primeiroOcteto = novaMascaraBinario.substring(0, 8);
+		String segundoOcteto = novaMascaraBinario.substring(8, 16);
+		String terceiroOcteto = novaMascaraBinario.substring(16, 24);
+		String quartoOcteto = novaMascaraBinario.substring(24, 32);
 		
 		setMascaraBinario(primeiroOcteto + '.' + segundoOcteto + '.' + terceiroOcteto + '.' + quartoOcteto);
 		
@@ -104,16 +106,49 @@ public class Ip {
 		
 	}
 	
+	private int definirIpsDisponiveis() {
+		
+		int bitsZero = 0;
+		
+		for(int i = 0; i < novaMascaraBinario.length(); i++) {
+			
+			if(novaMascaraBinario.charAt(i) == '0' ) {
+				bitsZero ++;
+			}
+		}
+		
+		int ipsDisponiveis = (int) ((Math.pow(2, bitsZero)) - 2);
+		
+		return ipsDisponiveis;
+	}
+	
+	private int definirSubRedes(){
+		
+       int bitsUm = 0;
+		
+		for(int i = 0; i < novaMascaraBinario.length(); i++) {
+			
+			if(novaMascaraBinario.charAt(i) == '1' ) {
+				bitsUm ++;
+			}
+		}
+		
+		int SubRedes = (int) Math.pow(2, bitsUm);
+		return SubRedes;
+	}
 
+	
 	
 	public void mostrarDados() {
 		
 		definirClasse();
 		definirMascara();
+		definirIpsDisponiveis();
 		
 		System.out.println("Classe: " + classe);
-		System.out.println("Mascara em Binário: " + mascaraBinario);
+		System.out.println("Mascara em Bin�rio: " + mascaraBinario);
 		System.out.println("Mascara em Decimal: " + mascaraDecimal);
+		System.out.println("Ips host dispon�veis: " + definirIpsDisponiveis());
 		System.out.println();
 	
 	}
